1bd1fd9f8b18f8d8565ebf1fd2ad25a93cecd8b4
David Holstius <david.holstius@gmail.com>
2020-05-13 10:58:18 -0700

    change replace_which() to replace()

--- R/recode_using.R
+++ R/recode_using.R
@@ -1,8 +1,16 @@
+#' recode_using
+#'
+#' @note Similar in purpose to `fct_recode`?
+#'
+#' @param x vector
+#' @param codec named integer vector
+#' @param other what to use for missing levels
+#'
 #' @export
 recode_using <- function (x, codec, other = "Other") {
   old <- c(names(codec), other)
   new <- c(unname(codec), other)
-  othered <- x %>% replace_which(. %not_in% old, other)
+  othered <- replace(x, !(x %in% old), other)
   recoded <- factor(othered, levels = old, labels = new)
   as(recoded, class(x))
 }
