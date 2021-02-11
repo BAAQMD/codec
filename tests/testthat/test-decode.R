5e551c779aba7a0129905cec6d01f76e9b7ec849
David Holstius <david.holstius@gmail.com>
2021-02-11 15:07:24 -0800

    tidy tests

--- tests/testthat/test-decode.R
+++ tests/testthat/test-decode.R
@@ -3,13 +3,23 @@ context("decode")
 ALPHABET <- setNames(1:26, LETTERS)
 
 test_that("numeric vector", {
-  expect_equal(decode(c(6, 15, 15), ALPHABET), c("F", "O", "O"))
+  expect_equal(
+    decode(
+      c(6, 15, 15),
+      ALPHABET),
+    c("F", "O", "O"))
 })
 
 test_that("integer vector", {
-  expect_equal(decode(c(2, 1, 18), ALPHABET), c("B", "A", "R"))
+  expect_equal(
+    decode(
+      c(2L, 1L, 18L),
+      c("A" = 1L, "B" = 2L, "R" = 18L)),
+    c("B", "A", "R"))
 })
 
 test_that("integers not in codec yield NA", {
-  expect_equal(decode(30, ALPHABET), NA_character_)
+  expect_equal(
+    decode(30, ALPHABET),
+    NA_character_)
 })
